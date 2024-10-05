5d6da9b0ff6f2e4ccfd9ca56f1eb2ec9f6c703af
ranadebsaha <ranadebsaha2002@gmail.com>
2024-10-05 23:07:21 +0530

    05/10/24 update data

--- api.js
+++ api.js
@@ -2,12 +2,23 @@ const express=require('express');
 const dbConnect=require('./mongodb');
 const app=express();
 
+app.use(express.json());
+
 app.get('/',async (req,resp)=>{
     let data=await dbConnect();
     data=await data.find().toArray();
     // console.log(data);
     // resp.send({name:'ranadeb'});
     resp.send(data);
+});
+
+app.post('/', async (req,resp)=>{
+    // console.log(req.body);
+    // resp.send({name:'ranadeb'});
+    // resp.send(req.body);
+    let data=await dbConnect();
+    let result= await data.insertOne(req.body);
+    resp.send(result);
 });
 
 app.listen(5000);
