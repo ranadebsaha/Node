d4e1ea37af95511d9e0020d2cb52c6be3e5dde8e
ranadebsaha <ranadebsaha2002@gmail.com>
2024-10-06 19:43:08 +0530

    06/10/24 update data

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const dbConnect=require('./mongodb');
 const app=express();
+const mongodb=require('mongodb');
 
 app.use(express.json());
 
@@ -21,4 +22,20 @@ app.post('/', async (req,resp)=>{
     resp.send(result);
 });
 
+app.put('/:name', async (req,resp)=>{
+    // console.log(req.body);
+    // resp.send({name:'rds'});
+    let data=await dbConnect();
+    // let result=await data.updateOne({name:req.body.name},{$set:req.body});
+    let result=await data.updateOne({name:req.params.name},{$set:req.body});
+    resp.send(result);
+});
+
+app.delete('/:id', async(req,resp)=>{
+    // console.log(req.params.id);
+    // resp.send('done');
+    let data= await dbConnect();
+    let result=await data.deleteOne({_id:new mongodb.ObjectId(req.params.id)});
+    resp.send(result);
+});
 app.listen(5000);
